89f27a69ca24cb99f4c420dde8f4d3c3f95ae942
SirMrPeter <piotrkarolambroziak@gmail.com>
2023-04-09 22:05:18 +0200

    fix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,16 +1,6 @@
 const allowedOrigins = [
-    'http://127.0.0.1:5500',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:3001',
-    'http://localhost:3500',
-    'http://127.0.0.1:80',
-    'http://localhost:80',
-    'http://127.0.0.1:443',
-    'http://localhost:443',
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'http://localhost:4000',
-    'http://192.168.0.74:3000',
+    'https://ud.ambro.dev',
+    'http://ud.ambro.dev',
     'https://admin.socket.io/'
 ];
 
